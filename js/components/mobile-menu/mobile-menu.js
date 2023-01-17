5580e86ef1dcf585b87343958278aac6a5149a41
Jurgita <jurgita.eicinaite@yahoo.com>
2023-01-17 17:17:46 +0200

    menu vertical bar on click

---
+++ js/components/mobile-menu/mobile-menu.js
@@ -0,0 +1,13 @@
+const hamburgerDOM = document.querySelector('.navbar-toggler');
+const navDOM = document.querySelector('.nav-bar-main');
+
+function hamburgerClick() {
+    hamburgerDOM.addEventListener('click', () => {
+        navDOM.classList.toggle('hidden');
+        navDOM.classList.toggle('clicked');
+
+        hamburgerDOM.classList.toggle('hidden');
+        console.log('clicked');
+    });
+}
+export { hamburgerClick };
