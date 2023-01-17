3d8b7c6a43802a3aa1c8463d9241f6e2bfb3136e
Jurgita <jurgita.eicinaite@yahoo.com>
2023-01-17 18:18:55 +0200

    correct hover - no display of drop dow

--- js/components/mobile-menu/mobile-menu.js
+++ js/components/mobile-menu/mobile-menu.js
@@ -1,12 +1,11 @@
 const hamburgerDOM = document.querySelector('.navbar-toggler');
 const navDOM = document.querySelector('.nav-bar-main');
+const navLinkDOM = document.querySelector('.nav-item');
 
 function hamburgerClick() {
     hamburgerDOM.addEventListener('click', () => {
         navDOM.classList.toggle('hidden');
         navDOM.classList.toggle('clicked');
-        hamburgerDOM.classList.toggle('hidden');
-        console.log('clicked');
     });
 }
 export { hamburgerClick };
