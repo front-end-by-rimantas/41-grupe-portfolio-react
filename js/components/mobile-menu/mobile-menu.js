6b63ff63922d76a8641185dd49e573dfa40724f4
Jurgita <jurgita.eicinaite@yahoo.com>
2023-01-18 17:26:55 +0200

    main logic

--- js/components/mobile-menu/mobile-menu.js
+++ js/components/mobile-menu/mobile-menu.js
@@ -1,17 +1,20 @@
+// hidden div for generating new nav accordint to desktop version
+const mobileDOM = document.getElementById('mobile-nav');
+// Coppy all meniu to new div
+mobileDOM.innerHTML =
+    `<div class="close">
+                    <a href="#" class="fa fa-times"></a>
+                </div>` + document.querySelector('.nav-bar-main').innerHTML;
 const hamburgerDOM = document.querySelector('.navbar-toggler');
-const navDOM = document.querySelector('.nav-bar-main');
-const navLinkDOM = document.querySelector('.nav-item');
-let clicked = true;
+const closeDOM = document.querySelector('.close');
+
 function hamburgerClick() {
     hamburgerDOM.addEventListener('click', () => {
-        navDOM.classList.toggle('hidden');
-        navDOM.classList.toggle('clicked');
-        /* window.addEventListener('resize', () => {
-            if (window.innerWidth === 992 && clicked) {
-                navDOM.classList.toggle('hidden');
-                navDOM.classList.toggle('clicked');
-            }
-        }); */
+        mobileDOM.classList.toggle('hidden');
     });
+    closeDOM.addEventListener('click', () => {
+        mobileDOM.classList.toggle('hidden');
+    });
+    console.log(mobileDOM.innerHTML);
 }
 export { hamburgerClick };
