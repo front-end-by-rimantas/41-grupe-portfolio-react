0544fe5af15fde6276aca11b35967e811c501e17
Jurgita <jurgita.eicinaite@yahoo.com>
2023-01-18 18:33:01 +0200

    working version (refactoring needed)

--- js/components/mobile-menu/mobile-menu.js
+++ js/components/mobile-menu/mobile-menu.js
@@ -9,6 +9,15 @@ mobileDOM.innerHTML =
 const hamburgerDOM = document.querySelector('.navbar-toggler');
 // close button
 const closeDOM = document.querySelector('.close');
+// select buttons having drop down
+const menuDOM = [
+    ...document.querySelectorAll('#mobile-nav .dropdown-item'),
+].map((a) => a.parentElement);
+
+// add hidden class to drop-down items
+document
+    .querySelectorAll('.navbar > #mobile-nav .dropdown-item')
+    .forEach((a) => a.classList.add('hidden'));
 
 function hamburgerClick() {
     hamburgerDOM.addEventListener('click', () => {
@@ -17,6 +26,15 @@ function hamburgerClick() {
     closeDOM.addEventListener('click', () => {
         mobileDOM.classList.toggle('hidden');
     });
+
+    for (let i = 0; i < menuDOM.length; i++) {
+        menuDOM[i].addEventListener('click', () => {
+            menuDOM[i].children[1].classList.toggle('hidden');
+            console.log(menuDOM[i]);
+        });
+    }
+
     // console.log(mobileDOM.innerHTML);
+    // console.log(menuDOM.length);
 }
 export { hamburgerClick };
