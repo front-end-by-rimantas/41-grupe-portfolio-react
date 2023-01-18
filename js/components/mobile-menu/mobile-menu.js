d4dd40dba1fcea506fb1907b6af574685b5a35bc
Jurgita <jurgita.eicinaite@yahoo.com>
2023-01-18 09:45:27 +0200

    minor change

--- js/components/mobile-menu/mobile-menu.js
+++ js/components/mobile-menu/mobile-menu.js
@@ -1,11 +1,17 @@
 const hamburgerDOM = document.querySelector('.navbar-toggler');
 const navDOM = document.querySelector('.nav-bar-main');
 const navLinkDOM = document.querySelector('.nav-item');
-
+let clicked = true;
 function hamburgerClick() {
     hamburgerDOM.addEventListener('click', () => {
         navDOM.classList.toggle('hidden');
         navDOM.classList.toggle('clicked');
+        /* window.addEventListener('resize', () => {
+            if (window.innerWidth === 992 && clicked) {
+                navDOM.classList.toggle('hidden');
+                navDOM.classList.toggle('clicked');
+            }
+        }); */
     });
 }
 export { hamburgerClick };
