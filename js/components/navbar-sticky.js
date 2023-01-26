2a3b4f671f7d305651f39067bc543b2d878ce9ff
audrbar <102819309+audrbar@users.noreply.github.com>
2023-01-26 12:35:28 +0200

    scrollnav

--- js/components/navbar-sticky.js
+++ js/components/navbar-sticky.js
@@ -1,39 +1,28 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () {
-    navSticky();
-};
+let navbar = document.querySelector('#home');
+let featuresSection = document.getElementById('features');
+let navPos = navbar.getBoundingClientRect().top;
 
-// Get the navbar
-let navbar = document.getElementById('navbar');
-
-// Get the position of the navbar
-let sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function navSticky() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  window.addEventListener('scroll', e => {
+      let scrollPos = window.scrollY;
+        if (scrollPos > navPos) {
+            navbar.classList.add('sticky');
+            features.classList.add('navbarOffsetMargin');
+        } else {
+            navbar.classList.remove('sticky');
+            features.classList.add('navbarOffsetMargin');
+        }
+    }
+});
+export { navSticky };
+// const navbar = document.querySelector('#home');
+// let top = navbar.offsetTop;
 // function navSticky() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add('sticky')
-//   } else {
-//     navbar.classList.remove('sticky');
-//   }
+//     if (window.scrollY >= top) {
+//         navbar.classList.add('sticky');
+//     } else {
+//         navbar.classList.remove('sticky');
+//     }
 // }
-// export { navSticky }
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     document.getElementById("navbar").style.top = "0";
-//   } else {
-//     document.getElementById("navbar").style.top = "-50px";
-//   }
-// }
+// window.addEventListener('scroll', navSticky);
+// export { navSticky };
