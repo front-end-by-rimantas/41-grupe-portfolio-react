b862408d987368ab0861c9aa38aa7a47600ca65b
audrbar <102819309+audrbar@users.noreply.github.com>
2023-01-26 16:42:47 +0200

    stickynav

--- js/components/navbar-sticky.js
+++ js/components/navbar-sticky.js
@@ -1,19 +1,33 @@
-let navbar = document.querySelector('#home');
-let featuresSection = document.getElementById('features');
-let navPos = navbar.getBoundingClientRect().top;
+window.onscroll = function () {
+    navSticky();
+};
+
+let navbar = document.getElementById('home');
+let sticky = navbar.offsetTop;
 
 function navSticky() {
-  window.addEventListener('scroll', e => {
-      let scrollPos = window.scrollY;
-        if (scrollPos > navPos) {
-            navbar.classList.add('sticky');
-            features.classList.add('navbarOffsetMargin');
-        } else {
-            navbar.classList.remove('sticky');
-            features.classList.add('navbarOffsetMargin');
-        }
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add('sticky');
+    } else {
+        navbar.classList.remove('sticky');
     }
-});
+}
+// let navbar = document.querySelector('#home');
+// let featuresSection = document.getElementById('features');
+// let navPos = navbar.getBoundingClientRect().top;
+
+// function navSticky() {
+//   window.addEventListener('scroll', e => {
+//       let scrollPos = window.scrollY;
+//         if (scrollPos > navPos) {
+//             navbar.classList.add('sticky');
+//             features.classList.add('navbarOffsetMargin');
+//         } else {
+//             navbar.classList.remove('sticky');
+//             features.classList.add('navbarOffsetMargin');
+//         }
+//     }
+// });
 export { navSticky };
 // const navbar = document.querySelector('#home');
 // let top = navbar.offsetTop;
